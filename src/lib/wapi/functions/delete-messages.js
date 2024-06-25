094e599dde9c7c121a6584fca98cf8b26fc6f866
Vinicius Oliveira <vinicius.developer@outlook.com>
2024-06-25 08:34:53 -0300

    fix: dev review

--- src/lib/wapi/functions/delete-messages.js
+++ src/lib/wapi/functions/delete-messages.js
@@ -3,62 +3,46 @@ export async function deleteMessages(chatId, messageArray) {
     return WAPI.scope(null, true, 404, 'enter the chatid variable as an string')
   }
 
-  if (messageArray.some((msg) => msg.includes('true'))) {
+  if (messageArray.some((msg) => !msg.includes('true'))) {
     return WAPI.scope(null, true, 404, 'Some messages are not valid')
   }
+
   const chat = await WAPI.sendExist(chatId)
 
-  if (chat && chat.status != 404) {
-    if (!Array.isArray(messageArray)) {
-      return WAPI.scope(
-        chat,
-        true,
-        404,
-        'enter the message identification variable as an array'
-      )
-    }
-
-    for (const i in messageArray) {
-      if (typeof messageArray[i] === 'string') {
-        const checkID = await WAPI.checkIdMessage(chatId, messageArray[i])
-        if (checkID?.erro && checkID?.erro == true) {
-          return checkID
-        }
-      }
-    }
-
-    const messagesToDelete = await Promise.all(
-      messageArray.map((msgId) => WAPI.getMessageById(msgId, null, false))
-    )
-
-    const To = chat.id
-    const m = { type: 'deleteMessages' }
-    try {
-      var result = await Store.sendRevokeMsgs(
-        chat,
-        {
-          list: messagesToDelete,
-        },
-        true
-      )
-
-      const obj = WAPI.scope(To, !!result, result, '')
-      Object.assign(obj, m)
-      return obj
-    } catch (e) {
-      const obj = WAPI.scope(
-        null,
-        true,
-        result,
-        'The messages has not been deleted'
-      )
-      Object.assign(obj, m)
-      return obj
-    }
-  } else {
-    if (!chat.erro) {
-      chat.erro = true
-    }
-    return chat
+  if (!chat || chat.status == 404) {
+    return WAPI.scope(chat, true, 404, 'Chat not found')
   }
+
+  if (!Array.isArray(messageArray)) {
+    return WAPI.scope(
+      chat,
+      true,
+      404,
+      'enter the message identification variable as an array'
+    )
+  }
+
+  const messagesToDelete = await Promise.all(
+    messageArray.map(async (msgId) => {
+      WAPI.getMessageById(msgId, null, false)
+    })
+  )
+
+  let error, result
+
+  try {
+    result = await Store.sendRevokeMsgs(
+      chat,
+      {
+        list: messagesToDelete,
+      },
+      true
+    )
+  } catch (e) {
+    error = e?.message || 'The messages has not been deleted'
+  }
+
+  const scope = await WAPI.scope(error ? null : chat.id, !!error, result, error)
+
+  return { type: 'deleteMessages', ...scope }
 }
